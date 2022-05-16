e4f279111bb1700b311f1733eccbf0ec7d8d61e9
dkalev_vm <damyan@valuemetrics.nl>
2022-05-16 11:03:01 +0200

    feat: inference on gpu

--- inference.py
+++ inference.py
@@ -1,15 +1,19 @@
 import argparse
 import json
-from PIL import Image
-
-from models import MobileNetV2
-from dataset import get_data_augs
 from pathlib import Path
+
+import torch
+from PIL import Image
 from torchvision.transforms import Compose
+
+from dataset import get_data_augs
+from models import MobileNetV2
 
 
 def predict(config: argparse.Namespace) -> None:
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     model = MobileNetV2.load_from_checkpoint(config.model_weights_url)
+    model.to(device)
     model.eval()
 
     with open("data/label_mapping.json") as f:
@@ -18,7 +22,7 @@ def predict(config: argparse.Namespace) -> None:
     image = Image.open(config.image_path).convert("RGB")
     data_augs = get_data_augs(Path("data"))
     transforms = Compose(data_augs["common"] + data_augs["image_net"])
-    image = transforms(image).unsqueeze(0)
+    image = transforms(image).unsqueeze(0).to(device)
 
     pred = model(image).argmax(dim=-1)
     print("The model predictions is: ", label_mapping[str(pred.item())])
